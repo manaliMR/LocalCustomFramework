f9cc958640e526b9c63808617d8f87064417deb9
Manali Rami <73849042+Manali92@users.noreply.github.com>
2021-01-05 15:54:14 -0500

    Finished Framework

---
+++ LocalCustomFramework/LocalCustomFramework/LocalCustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  LocalCustomFramework.h
+//  LocalCustomFramework
+//
+//  Created by Manali Rami on 2021-01-05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalCustomFramework.
+FOUNDATION_EXPORT double LocalCustomFrameworkVersionNumber;
+
+//! Project version string for LocalCustomFramework.
+FOUNDATION_EXPORT const unsigned char LocalCustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalCustomFramework/PublicHeader.h>
+
+
---
+++ LocalCustomFramework/LocalCustomFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  LocalCustomFramework
+//
+//  Created by Manali Rami on 2021-01-05.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did something great"
+    }
+}
